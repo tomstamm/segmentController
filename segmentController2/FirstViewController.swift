2c479f6871a7e502ef0f011bbc1430cc3fdb17d1
Tom Stamm <tom.stamm@unionbank.com>
2019-12-23 15:17:45 -0800

    Initial Commit

---
+++ segmentController2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  segmentController2
+//
+//  Created by Tom Stamm on 12/23/19.
+//  Copyright © 2019 Tom Stamm. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ segmentController2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  segmentController2
+//
+//  Created by Tom Stamm on 12/23/19.
+//  Copyright © 2019 Tom Stamm. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
